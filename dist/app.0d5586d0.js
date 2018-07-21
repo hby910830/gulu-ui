c78429f9a48d4809a57925855e8558ee3ba4388c
hanbaoyi <604594231@qq.com>
2018-07-22 01:29:23 +0800

    创建 button group

--- dist/app.0d5586d0.js
+++ dist/app.0d5586d0.js
@@ -10475,6 +10475,59 @@ render._withStripped = true
       
       }
     })();
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"button-group.vue":[function(require,module,exports) {
+//
+//
+//
+//
+//
+        var $dca4b1 = exports.default || module.exports;
+      
+      if (typeof $dca4b1 === 'function') {
+        $dca4b1 = $dca4b1.options;
+      }
+    
+        /* template */
+        Object.assign($dca4b1, (function () {
+          var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "g-button-group" }, [_vm._t("default")], 2)
+}
+var staticRenderFns = []
+render._withStripped = true
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: null,
+            functional: undefined
+          };
+        })());
+      
+    /* hot reload */
+    (function () {
+      if (module.hot) {
+        var api = require('vue-hot-reload-api');
+        api.install(require('vue'));
+        if (api.compatible) {
+          module.hot.accept();
+          if (!module.hot.data) {
+            api.createRecord('$dca4b1', $dca4b1);
+          } else {
+            api.reload('$dca4b1', $dca4b1);
+          }
+        }
+
+        
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+      }
+    })();
 },{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"app.js":[function(require,module,exports) {
 'use strict';
 
@@ -10490,10 +10543,15 @@ var _icon = require('./icon');
 
 var _icon2 = _interopRequireDefault(_icon);
 
+var _buttonGroup = require('./button-group');
+
+var _buttonGroup2 = _interopRequireDefault(_buttonGroup);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _vue2.default.component('g-button', _button2.default);
 _vue2.default.component('g-icon', _icon2.default);
+_vue2.default.component('g-button-group', _buttonGroup2.default);
 
 new _vue2.default({
     el: '#app',
@@ -10503,7 +10561,7 @@ new _vue2.default({
         loading3: false
     }
 });
-},{"vue":"node_modules/vue/dist/vue.common.js","./button":"button.vue","./icon":"icon.vue"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"vue":"node_modules/vue/dist/vue.common.js","./button":"button.vue","./icon":"icon.vue","./button-group":"button-group.vue"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
