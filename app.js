75ee03382525d5e26ae15d3bcb1b35c9d07abd44
hanbaoyi <604594231@qq.com>
2019-02-06 13:00:05 +0800

    使用chai.expect添加四个测试用例

--- app.js
+++ app.js
@@ -8,10 +8,64 @@ Vue.component('y-icon', Icon)
 Vue.component('y-button-group', ButtonGroup)
 
 new Vue({
-    el: '#app',
-    data: {
-        loading1: false,
-        loading2: true,
-        loading3: false,
-    }
+  el: '#app',
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false,
+  }
 })
+
+
+//单元测试
+import chai from 'chai'
+
+const expect = chai.expect
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true,
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} =window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
