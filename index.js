ec6e239670546469a583d98f43f60eecbc9f1432
hanbaoyi <604594231@qq.com>
2019-03-11 16:50:17 +0800

    Refresh adding .gitignore file.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import icon from './src/icon'
+import Col from './src/col'
+import Row from './src/row'
 
-export {Button, ButtonGroup, icon}
+export {Button, ButtonGroup, icon, Col, Row}
