44bf8abf97c085aeb001d44591a53f63a41a13db
hanbaoyi <604594231@qq.com>
2019-03-11 16:38:01 +0800

    col 和 row 的雏形完成

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import Col from './col'
+import Row from './row'
 import ButtonGroup from './button-group'
 
 Vue.component('y-button', Button)
+Vue.component('y-col', Col)
+Vue.component('y-row', Row)
 Vue.component('y-icon', Icon)
 Vue.component('y-button-group', ButtonGroup)
 
---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GuluRow"
+  }
+</script>
+
+<style scoped lang="scss">
+  .row {
+    display: flex;
+  }
+</style>
