3612c1e8ae33793e99bf439aa6035515fd96940b
hanbaoyi <604594231@qq.com>
2019-03-30 15:33:47 +0800

    实现最简单的popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="popover">
-    <div class="content-wrapper" v-if="visible">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
-    <div @click="xxx">
+    <div ref="triggerWrapper" @click="xxx">
       <slot></slot>
     </div>
   </div>
@@ -17,9 +17,25 @@
         visible: false
       }
     },
+    mounted(){
+    },
     methods: {
       xxx() {
         this.visible = !this.visible
+        if (this.visible) {
+          setTimeout(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width,left,top} = this.$refs.triggerWrapper.getBoundingClientRect()
+            console.log(width, left, top);
+            this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`
+            this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`
+            let eventHandler = () => {
+              this.visible = false
+              document.removeEventListener('click', eventHandler)
+            }
+            document.addEventListener('click', eventHandler)
+          }, 300)
+        }
       }
     }
   }
@@ -30,12 +46,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-    }
+  }
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+    transform: translateY(-100%);
   }
 </style>
