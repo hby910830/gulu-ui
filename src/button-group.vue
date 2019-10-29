da62661a868b471213bcf408d14f19dbf61767c3
hanbaoyi <604594231@qq.com>
2019-10-29 23:17:20 +0800

    修改button.vue的html变量为scss变量

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,41 +1,42 @@
 <template>
-    <div class="y-button-group">
-        <slot></slot>
-    </div>
+  <div class="y-button-group">
+    <slot></slot>
+  </div>
 </template>
 <script>
-    export default {
-        mounted() {
-            for (let node of this.$el.children) {
-                let name = node.nodeName.toLocaleLowerCase()
-                if (name !== 'button') {
-                    console.warn(`y-button-group 的子元素应该全是 y-button，但是你写的是 ${name}`)
-                }
-            }
-        }
-    }
+	export default {
+		mounted() {
+			for (let node of this.$el.children) {
+				let name = node.nodeName.toLocaleLowerCase()
+				if (name !== 'button') {
+					console.warn(`y-button-group 的子元素应该全是 y-button，但是你写的是 ${name}`)
+				}
+			}
+		}
+	}
 </script>
 <style lang="scss">
-    .y-button-group {
-        display: inline-flex;
-        vertical-align: middle;
-        > .y-button {
-            border-radius: 0;
-            &:not(:first-child) {
-                margin-left: -1px;
-            }
-            &:first-child {
-                border-top-left-radius: var(--border-radius);
-                border-bottom-left-radius: var(--border-radius);
-            }
-            &:last-child {
-                border-top-right-radius: var(--border-radius);
-                border-bottom-right-radius: var(--border-radius);
-            }
-            &:hover {
-                position: relative;
-                z-index: 1;
-            }
-        }
+  $border-radius: 4px;
+  .y-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .y-button {
+      border-radius: 0;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
+        border-top-left-radius: $border-radius;
+        border-bottom-left-radius: $border-radius;
+      }
+      &:last-child {
+        border-top-right-radius: $border-radius;
+        border-bottom-right-radius: $border-radius;
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
     }
+  }
 </style>
--- src/button.vue
+++ src/button.vue
@@ -9,29 +9,37 @@
   </button>
 </template>
 <script>
-  import Icon from './icon'
+	import Icon from './icon'
 
-  export default {
-    components: {
-      'y-icon': Icon
-    },
-    props: {
-      icon: {},
-      iconPosition: {
-        type: String,
-        default: 'left',
-        validator(value) {
-          return value === 'left' || value === 'right'
-        }
-      },
-      loading: {
-        type: Boolean,
-        default: false
-      }
-    }
-  }
+	export default {
+		components: {
+			'y-icon': Icon
+		},
+		props: {
+			icon: {},
+			iconPosition: {
+				type: String,
+				default: 'left',
+				validator(value) {
+					return value === 'left' || value === 'right'
+				}
+			},
+			loading: {
+				type: Boolean,
+				default: false
+			}
+		}
+	}
 </script>
 <style lang="scss">
+  $button-height: 32px;
+  $font-size: 14px;
+  $button-bg: white;
+  $button-active-bg: #eee;
+  $border-radius: 4px;
+  $color: #333;
+  $border-color: #999;
+  $border-color-hover: red;
   @keyframes spin {
     0% {
       transform: rotate(0deg);
@@ -40,23 +48,22 @@
       transform: rotate(360deg);
     }
   }
-
   .y-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
+    font-size: $font-size;
+    height: $button-height;
     padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
     display: inline-flex;
     justify-content: center;
     align-items: center;
     vertical-align: middle;
     &:hover {
-      border-color: var(--border-color-hover);
+      border-color: $border-color-hover;
     }
     &:active {
-      background-color: var(--button-active-bg);
+      background-color: $button-active-bg;
     }
     &:focus {
       outline: none;
