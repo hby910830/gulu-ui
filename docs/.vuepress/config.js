b54da0a441397d09912ceefe87e4c2e4030a52cf
hanbaoyi <604594231@qq.com>
2019-10-29 23:00:23 +0800

    add themeConfig的sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,13 @@
 module.exports = {
 	title: '轱辘 UI ',
-	description: '一个好用的Vue UI 框架'
+	description: '一个好用的Vue UI 框架',
+	themeConfig:{
+		nav:[
+			{}
+		],
+		sidebar: [
+			'/install/',
+			'/get-started/'
+		]
+	}
 }
--- docs/install/README.md
+++ docs/install/README.md
@@ -1,3 +1,5 @@
-# 轱辘 UI 官网
+---
+title: 安装
+---
 
-轱辘是一个好用的轻量级的 UI 框架， 提供了Button/Toast/Popover/Input/Layout/Tabs等常用组件
+# 安装
