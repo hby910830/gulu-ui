cb8e2ab07f7b8a56f9b75a4be8b49421b46be5eb
hanbaoyi <604594231@qq.com>
2019-10-31 23:34:32 +0800

    使用<pre><code></code></pre>在文档中展示代码

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <y-button>默认按钮</y-button>
+    <y-button disabled>默认按钮</y-button>
+    <y-button icon="settings">默认按钮</y-button>
+    <y-button :loading="true">默认按钮</y-button>
+
+    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 
@@ -8,6 +13,16 @@
 	import Button from '../../../src/button'
 
 	export default {
-		components: {'y-button': Button}
+		components: {'y-button': Button},
+		data() {
+			return {
+				content: `
+                <y-button>默认按钮</y-button>
+                <y-button disabled>默认按钮</y-button>
+                <y-button icon="settings">默认按钮</y-button>
+                <y-button :loading="true">默认按钮</y-button>
+				`
+			}
+		}
 	}
 </script>
