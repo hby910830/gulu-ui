eaf8ab979ba4d92d13305af37795ef81de363334
hanbaoyi <604594231@qq.com>
2020-02-06 23:18:20 +0800

    src/plugin.js

--- src/plugin.js
+++ src/plugin.js
@@ -1,29 +1,31 @@
 import Toast from './toast'
+
 let currentToast
 export default {
-  install(Vue) {
-    Vue.prototype.$toast = function (message, options) {
-      if (currentToast) {
-        currentToast.close()
-      }
-      currentToast = createToast({
-        Vue,
-        message,
-        propsData: options,
-        onClose: () => {
-          currentToast = null
-        }
-      })
-    }
-  }
+	install(Vue) {
+		Vue.prototype.$toast = function (message, options) {
+			if (currentToast) {
+				currentToast.close()
+			}
+			currentToast = createToast({
+				Vue,
+				message,
+				propsData: options,
+				onClose: () => {
+					currentToast = null
+				}
+			})
+		}
+	}
 }
+
 // helper
 function createToast({Vue, message, propsData, onClose}) {
-  let Constructor = Vue.extend(Toast)  //创造Toast子类的构造器
-  let toast = new Constructor({propsData})
-  toast.$slots.default = message //设置组建的默认slots
-  toast.$mount()
-  toast.$on('close', onClose)
-  document.body.appendChild(toast.$el)
-  return toast
+	let Constructor = Vue.extend(Toast)  //创造Toast子类的构造器
+	let toast = new Constructor({propsData})
+	toast.$slots.default = message //设置组建的默认slots
+	toast.$mount()
+	toast.$on('close', onClose)
+	document.body.appendChild(toast.$el)
+	return toast
 }
