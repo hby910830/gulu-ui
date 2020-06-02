49b346cfe76b2148303fd13842f2f494c536b897
hanbaoyi <604594231@qq.com>
2020-06-02 23:50:03 +0800

    递归算法解决右边不更新问题

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="cascaderItems" :style="{height}">
 		<div class="left">
-			<div class="label" v-for="item in items" @click="onClickSelected(item)">
+			<div class="label" v-for="item in items" @click="onClickLabel(item)">
 				<span class="name">{{item.name}}</span>
 				<span v-if="item.children"> > </span>
 			</div>
@@ -10,6 +10,7 @@
 			<gulu-cascader-items :level="level + 1" :items="rightItems"
 													 :selected="selected"
 													 :height="height"
+													 :load-data="loadData"
 													 @update:selected="onUpdateSelected"
 			></gulu-cascader-items>
 		</div>
@@ -30,6 +31,9 @@
 				type: Array,
 				default: () => []
 			},
+			loadData: {
+				type: Function
+			},
 			level: {
 				type: Number,
 				default: 0
@@ -37,19 +41,19 @@
 		},
 		computed: {
 			rightItems() {
-				const currentSelected = this.selected && this.selected[this.level]
-				if (currentSelected && currentSelected.children) {
-					return currentSelected.children
-				} else {
-					return null
+				if (this.selected[this.level]) {
+					let selected = this.items.filter((item) => item.name === this.selected[this.level].name)
+					if (selected && selected[0].children && selected[0].children.length > 0) {
+						return selected[0].children
+					}
 				}
 			}
 		},
 		methods: {
-			onClickSelected(item) {
-				const copy = JSON.parse(JSON.stringify(this.selected))
+			onClickLabel(item) {
+				let copy = JSON.parse(JSON.stringify(this.selected))
 				copy[this.level] = item
-				copy.splice(this.level + 1)	//每次改变就清空后面的选项
+				copy.splice(this.level + 1) // 一句话
 				this.$emit('update:selected', copy)
 			},
 			onUpdateSelected(newSelected) {
@@ -64,7 +68,8 @@
 	.cascaderItems {display: flex;justify-content: flex-start;align-items: flex-start;height: 100px;
 		.left { height: 100%;padding: .3em 0;overflow: auto}
 		.right {height: 100%;border-left: 1px solid $border-color-light;}
-		.label { padding: .5em 1em;display: flex;align-items: center;cursor: pointer;white-space: nowrap; &:hover {background: $grey;}
+		.label { padding: .5em 1em;display: flex;align-items: center;cursor: pointer;white-space: nowrap;
+			&:hover {background: $grey;}
 			> .name {margin-right: 1em;user-select: none;}
 		}
 	}
--- src/demo.vue
+++ src/demo.vue
@@ -1,7 +1,10 @@
 <template>
 	<div>
 		<div style="padding: 20px">
-			<y-cascader :source="source" popover-height="200px" :selected.sync="selected"></y-cascader>
+			<y-cascader :source.sync="source" popover-height="200px"
+									:selected.sync="selected"
+									:loadData="loadData"
+			></y-cascader>
 		</div>
 	</div>
 </template>
@@ -10,8 +13,20 @@
 	import Cascader from './cascader'
 	import db from './db'
 
-	function ajax(parentId = 0) {
-		return db.filter(item => item.parent_id === parentId)
+	function ajax(parentId = 0, success) {	//回调
+		return setTimeout(() => {
+			const result = db.filter(item => item.parent_id === parentId)
+			return success(result)
+		}, 1000)
+	}
+
+	function ajax2(parentId = 0) {
+		return new Promise((resolve) => {	//Promise
+			setTimeout(() => {
+				const result = db.filter(item => item.parent_id === parentId)
+				resolve(result)
+			}, 1000)
+		})
 	}
 
 	export default {
@@ -20,8 +35,29 @@
 		data() {
 			return {
 				selected: [],
-				source: ajax()
+				source: []
 			}
+		},
+		created() {
+			// ajax(0, result => {
+			// 	this.source = result
+			// })
+			ajax2(0).then(result => {
+				this.source = result
+			})
+		},
+		methods:{
+			loadData({id}, updateSource){
+				ajax2(id).then(result => {
+					updateSource(result)
+				})
+			},
+			xxx(){
+				ajax2(this.selected[0].id).then(result => {
+					let lastLevelSelected = this.source.filter(item => item.id === this.selected[0].id)[0]
+					this.$set(lastLevelSelected, 'children', result)
+				})
+			},
 		}
 	}
 </script>
