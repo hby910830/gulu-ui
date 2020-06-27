6678a3f84c34a5408ad7f36d4c43ab3e1f26cd14
hanbaoyi <604594231@qq.com>
2020-06-28 00:17:26 +0800

    优化代码

--- src/nav/nav.vue
+++ src/nav/nav.vue
@@ -18,33 +18,39 @@
 			}
 		},
 		mounted() {
-			this.items.forEach(vm => {
-				vm.selected = this.selected.indexOf(vm.name) > -1;
-			})
-			this.items.forEach(vm => {
-				vm.$on('add:selected', name => {
-					if(this.multiple){
-						if (this.selected.indexOf(name) < 0) {
-							let copy = JSON.parse(JSON.stringify(this.selected))
-							copy.push(name)
-							this.$emit('update:selected', copy)
-						}
-					}else{
-						this.$emit('update:selected', [name])
-					}
-				})
-			})
+			this.updateChildren()
+			this.listenToChildren()
 		},
 		updated() {
-			this.items.forEach(vm => {
-				vm.selected = this.selected.indexOf(vm.name) > -1;
-			})
+			this.updateChildren()
 		},
 		computed: {
 			items() {
 				return this.$children.filter(vm => vm.$options.name === 'GuluNavItem')
 			}
 		},
+		methods: {
+			updateChildren() {
+				this.items.forEach(vm => {
+					vm.selected = this.selected.indexOf(vm.name) > -1
+				})
+			},
+			listenToChildren(){
+				this.items.forEach(vm => {
+					vm.$on('add:selected', name => {
+						if (this.multiple) {
+							if (this.selected.indexOf(name) < 0) {
+								let copy = JSON.parse(JSON.stringify(this.selected))
+								copy.push(name)
+								this.$emit('update:selected', copy)
+							}
+						} else {
+							this.$emit('update:selected', [name])
+						}
+					})
+				})
+			}
+		}
 	}
 </script>
 
