7d14960c070cffe3c6e78b7339f3321707d91057
hanbaoyi <604594231@qq.com>
2020-08-09 23:06:03 +0800

    实现nav基本结构

--- src/demo.vue
+++ src/demo.vue
@@ -2,7 +2,12 @@
 	<div>
 		<y-nav :selected.sync="selected">
 			<y-nav-item name="home">首页</y-nav-item>
-			<y-nav-item name="about">关于</y-nav-item>
+			<y-sub-nav>
+				<template slot="title">关于</template>
+				<y-nav-item name="culture">企业文化</y-nav-item>
+				<y-nav-item name="developers">开发团队</y-nav-item>
+				<y-nav-item name="contacts">联系电话</y-nav-item>
+			</y-sub-nav>
 			<y-nav-item name="hire">招聘</y-nav-item>
 		</y-nav>
 	</div>
--- src/nav/sub-nav.vue
+++ src/nav/sub-nav.vue
@@ -1,15 +1,44 @@
 <template>
-	<div class="y-sub-item">
-		<slot></slot>
+	<div class="y-sub-nav">
+		<span @click="onClick">
+			<slot name="title"></slot>
+		</span>
+		<div class="y-sub-nav-popover" v-show="open">
+			<slot></slot>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'GuluSubNav'
+		name: 'GuluSubNav',
+		data(){
+			return {
+				open: false
+			}
+		},
+		methods:{
+			onClick(){
+				this.open = !this.open
+			}
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.y-sub-item {}
+	.y-sub-nav {
+		position: relative;
+		> span{
+			padding: 10px 20px;
+			display: inline-flex;
+			vertical-align: top;
+		}
+		&-popover{
+			position: absolute;
+			top: 100%;
+			left: 0;
+			border: 1px solid black;
+			white-space: nowrap;
+		}
+	}
 </style>
